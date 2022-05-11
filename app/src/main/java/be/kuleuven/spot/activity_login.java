1e3dd7bb60a04a3e0db882bc3c352badc0b60b81
William <william.zhu@student.kuleuven.be>
2022-05-11 14:33:38 +0200

    first commit

--- app/src/main/java/be/kuleuven/spot/activity_login.java
+++ app/src/main/java/be/kuleuven/spot/activity_login.java
@@ -1,11 +1,27 @@
 package be.kuleuven.spot;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,12 +31,97 @@ import java.sql.Statement;
 
 public class activity_login extends AppCompatActivity {
 
+    private EditText inputEmail_login;
+    private EditText inputPassword_login;
     private TextView DoNotHaveAccount;
+
+    private RequestQueue requestQueue;
+    private static final String checkEmailExistUrl = "https://studev.groept.be/api/a21pt215/ifEmailExist/";
+    private static final String checkPasswordUrl = "https://studev.groept.be/api/a21pt215/passwordWithEmail/";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inputEmail_login = (EditText) findViewById(R.id.inputEmail_login);
+        inputPassword_login = (EditText) findViewById(R.id.inputPassword_login);
+    }
+
+    public void onLogin_Clicked(View caller) {
+
+        String email = inputEmail_login.getText().toString();
+        String password = inputPassword_login.getText().toString();
+        //Toast.makeText(activity_register.this, password + ", " + confirmPassword , Toast.LENGTH_LONG).show();
+        checkIfEmailPassword(email,password);
+    }
+
+    public void checkIfEmailPassword(String email, String password){
+        requestQueue = Volley.newRequestQueue(this);
+        String emailUrl = checkEmailExistUrl + email;
+        String passwordUrl = checkPasswordUrl + email;
+
+        JsonArrayRequest passwordRequest = new JsonArrayRequest(Request.Method.GET,passwordUrl,null,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                String info = "";
+                JSONObject o = null;
+                try {
+                    o = response.getJSONObject(0);
+                    info = (String)o.get("password");
+                    //System.out.println(info);
+                    //System.out.println(password + " ahoaho");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if(info.equals(password)){
+                    Intent intent = new Intent(activity_login.this, activity_post.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(activity_login.this, "Password is not correct" , Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity_login.this, "Unable to communicate with the server", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Email existence check
+        JsonArrayRequest emailRequest = new JsonArrayRequest(Request.Method.GET,emailUrl,null,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                int info = 0;
+                JSONObject o = null;
+                try {
+                    o = response.getJSONObject(0);
+                    info = parseInt((String) o.get("COUNT(email)"));
+                    //System.out.println(info + " is the value");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if(info == 0){
+                    Toast.makeText(activity_login.this, "This email is not registered" , Toast.LENGTH_LONG).show();
+
+                }else{
+                    requestQueue.add(passwordRequest);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity_login.this, "Unable to communicate with the server", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        requestQueue.add(emailRequest);
+
     }
 
     public void onDoNotHaveAccount_Clicked(View caller) {
@@ -29,25 +130,4 @@ public class activity_login extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void checkLoginData(){
-        Connection c = null;
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            c = DriverManager.getConnection("mysql.studev.groept.be","a21pt215","secret");
-            System.out.println("Opened database successfully");
-
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("");
-
-            while(rs.next() == true){
-                // write the action you wanna make with the data taken from the databse
-            }
-            rs.close();
-            stmt.close();
-            c.close();
-        }
-        catch(Exception e){
-            System.err.println("ERROR : " + e.getMessage());
-        }
-    }
 }
--- app/src/main/java/be/kuleuven/spot/activity_register.java
+++ app/src/main/java/be/kuleuven/spot/activity_register.java
@@ -1,16 +1,16 @@
 package be.kuleuven.spot;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.sql.Connection;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,31 +24,33 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 
 public class activity_register extends AppCompatActivity {
 
-    private TextView inputUsername;
-    private TextView inputEmail;
-    private TextView inputPassword;
-    private TextView confirmPassword;
+    private EditText inputUsername;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText inputConfirmPassword;
+    private Button btnRegister;
 
     private TextView AlreadyHaveAccount;
     private RequestQueue requestQueue;
     private static final String insertUrl = "https://studev.groept.be/api/a21pt215/Insert/";
+    private static final String checkUsernameExistUrl = "https://studev.groept.be/api/a21pt215/ifUsernameExist/";
+    private static final String checkEmailExistUrl = "https://studev.groept.be/api/a21pt215/ifEmailExist/";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        AlreadyHaveAccount = (TextView)findViewById(R.id.AlreadyHaveAccount);
-        
+        AlreadyHaveAccount = (TextView) findViewById(R.id.AlreadyHaveAccount);
+        inputUsername = (EditText) findViewById(R.id.inputEmail_login);
+        inputEmail = (EditText) findViewById(R.id.inputPassword_login);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+        inputConfirmPassword = (EditText) findViewById(R.id.inputConfirmPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
     }
 
     public void onAlreadyHaveAccount_Clicked(View caller) {
@@ -59,15 +61,34 @@ public class activity_register extends AppCompatActivity {
 
     public void onRegister_Clicked(View caller) {
 
-        passRegistrationData("Shusaku","12345");
-        Intent intent = new Intent(this, activity_post.class);
-        startActivity(intent);
+        String username = inputUsername.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConfirmPassword.getText().toString();
+        //Toast.makeText(activity_register.this, password + ", " + confirmPassword , Toast.LENGTH_LONG).show()
+
+        if(username == null || email == null || password == null || confirmPassword == null){
+            Toast.makeText(activity_register.this, "Please fill all", Toast.LENGTH_LONG).show();
+        }
+
+        if (password.length() < 8) {
+            Toast.makeText(activity_register.this, "Password has to be longer the 8 characters", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!(password.equals(confirmPassword))) {
+            Toast.makeText(activity_register.this, "Password does not match. Please try again", Toast.LENGTH_LONG).show();
+            return;
+        }
+        //Toast.makeText(activity_register.this, username, Toast.LENGTH_LONG).show();
+
+        //This method checks if the email or username is already existing in the database.
+        //In case it is not existing, it passes the parameters to the database with the method passRegistrationData
+        checkIfAlreadyExist(username, email, password, confirmPassword);
     }
 
-    public void passRegistrationData(String username, String password){
+    public void passRegistrationData(String username, String email, String password, String confirmPassword){
         //Log.d("Database","Creating request");
-        requestQueue = Volley.newRequestQueue(this);
-        String requestURL = insertUrl + username + "/" + password;
+        String requestURL = insertUrl + username + "/" + email + "/" + password;
         //Log.d("Database","Creating request");
 
         StringRequest submitRequest = new StringRequest(Request.Method.GET, requestURL,
@@ -75,17 +96,88 @@ public class activity_register extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
 
-                        Toast.makeText(activity_register.this, "Sccucess", Toast.LENGTH_LONG).show();
                     }
                 },
 
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(activity_register.this, "Unable to place the order", Toast.LENGTH_LONG).show();
+                        Toast.makeText(activity_register.this, "Unable to register", Toast.LENGTH_LONG).show();
                     }
                 }
         );
         requestQueue.add(submitRequest);
+        Intent intent = new Intent(this, activity_post.class);
+        startActivity(intent);
     }
+
+
+    public void checkIfAlreadyExist(String username, String email, String password, String confirmPassword) {
+
+        requestQueue = Volley.newRequestQueue(this);
+        String usernameUrl = checkUsernameExistUrl + username;
+        String emailUrl = checkEmailExistUrl + email;
+
+        //Username existence check
+        JsonArrayRequest usernameRequest = new JsonArrayRequest(Request.Method.GET,usernameUrl,null,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                int info = 0;
+
+                    JSONObject o = null;
+                    try {
+                        o = response.getJSONObject(0);
+                        info = parseInt((String) o.get("COUNT(username)"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                if(info>0){
+                    Toast.makeText(activity_register.this, "This username is already used", Toast.LENGTH_LONG).show();
+                    return;
+                }else{
+                    passRegistrationData(username,email,password,confirmPassword);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity_register.this, "Unable to communicate with the server", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        //Email Existence check
+        JsonArrayRequest emailRequest = new JsonArrayRequest(Request.Method.GET,emailUrl,null,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                int info = 0;
+                JSONObject o = null;
+                try {
+                    o = response.getJSONObject(0);
+                    info = parseInt((String) o.get("COUNT(email)"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //Toast.makeText(activity_register.this, info , Toast.LENGTH_LONG).show();
+                if(info>0){
+                    Toast.makeText(activity_register.this, "This email is already registered" , Toast.LENGTH_LONG).show();
+                    return;
+                }else{
+                    requestQueue.add(usernameRequest);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity_register.this, "Unable to communicate with the server", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        requestQueue.add(emailRequest);
+
+    }
+
 }

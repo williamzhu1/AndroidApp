ee73310bcacd6fab73095986cd55def7a48c426c
DESKTOP-74B8BQ8\segas <shusaku.segawa@student.kuleuven.be>
2022-05-27 23:33:10 +0200

    GPS

--- app/src/main/java/be/kuleuven/spot/ProfileFragment.java
+++ app/src/main/java/be/kuleuven/spot/ProfileFragment.java
@@ -1,19 +1,24 @@
 package be.kuleuven.spot;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +28,13 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TextView pf_username_top;
+    private TextView pf_username;
+    private TextView pf_email;
+    private Button btn_changePassword;
+
+    private String email;
+    private String username;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -49,16 +61,46 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        pf_username_top = (TextView) getView().findViewById(R.id.pf_username_top);
+        pf_username = (TextView) getView().findViewById(R.id.pf_username);
+        pf_email = (TextView)getView().findViewById(R.id.pf_email);
+        btn_changePassword = (Button) getView().findViewById(R.id.btn_changePassword);
+        btn_changePassword.setOnClickListener(this);
+
+        email = getActivity().getIntent().getExtras().getString("email");
+        username = getActivity().getIntent().getExtras().getString("username");
+        //Toast.makeText(getActivity(), email , Toast.LENGTH_LONG).show();
+
+        pf_email.setText(email);
+        pf_username.setText(username);
+        pf_username_top.setText(username);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), activity_changePassword.class);
+        intent.putExtras(getActivity().getIntent().getExtras());
+        startActivity(intent);
     }
 }
